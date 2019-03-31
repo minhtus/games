e4c8a5d39e5e7e260bfa6f9a7062d0d784d1f6b2
mtu98 <mitu9811@gmail.com>
2019-03-31 22:43:29 +0700

    Load games dynamically and list out

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-var $ = require('jquery');
+var user = 'mtu98';
+var repo = 'games';
+var git_api = `https://api.github.com/repos/${user}/${repo}/contents/`
+
+$.ajax(git_api, {
+    contentType: 'application/json',
+}).done((result) => {
+    for (var i = 0; i < result.length; i++) {
+        if ('dir' === result[i].type) {
+            var game = result[i].name;
+            var games = document.getElementById('games');
+            var li = document.createElement('li');
+            var a = document.createElement('a');
+            a.textContent = game;
+            a.setAttribute('href',`/${result[i].name}`);
+            li.append(a);
+            games.appendChild(li);
+        }
+    }
+});
+
