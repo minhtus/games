44eb33e5066e10cb2ece2f3e90b829932cb510d2
mtu98 <mitu9811@gmail.com>
2019-04-22 17:35:57 +0700

    Adjust ids, add switch turn and 2D array to manage board

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -1,25 +1,57 @@
-const cells = document.querySelectorAll('.cell')
-const board = Array.from(Array(9).keys());
+const cells = document.querySelectorAll('.cell');
+const n = 3;
+// Init a 2D array that manage the board
+const board = Array.from({ length: n }, () =>
+    Array.from({ length: n }, () => 0)
+);
+let player = 'O';
 function restart() {
-    for (var i = 0; i < cells.length; i++) {
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].textContent = '';
         cells[i].addEventListener('click', cellClick, false);
     }
+    board.forEach(arr => arr.fill(0));
+    console.log(board)
 }
 
 function cellClick(cell) {
-    console.log(cell.target.id);
-    play(cell.target.id, 'O')
+    play(cell.target.id, player);
 }
 
 function play(cellId, pattern) {
-    var cell = document.getElementById(cellId)
-    if (cell.textContent == "") {
-        console.log('play')
+    const cell = document.getElementById(cellId);
+    if (cell.textContent === '') {
+        console.log(`play ${player} on ${cellId}`);
         cell.textContent = pattern;
+        if (player === 'O') {
+            // value of 1 is O
+            writeBoard(cellId, 1);
+        } else {
+            // value of -1 is X
+            writeBoard(cellId, -1);
+        }
+    }
+    checkWin();
+    switchTurn();
+}
+
+function writeBoard(cellId, value) {
+    const row = cellId.charAt(1);
+    const col = cellId.charAt(2);
+    console.log(`${row}:${col}`)
+    board[row][col] = value;
+}
+
+function switchTurn() {
+    if (player === 'O') {
+        player = 'X';
+    } else {
+        player = 'O';
     }
 }
 
-function checkWin() {
+
+function checkWin(lastMoveRow, lastMoveCol) {
 
 }
 
