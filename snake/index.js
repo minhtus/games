5f59a9626d3d6619038f511bb8e5513aaf7ec84a
Nguyen Minh Tu <mitu9811@gmail.com>
2020-09-23 18:03:48 +0700

    Complete basic snake game

--- snake/index.js
+++ snake/index.js
@@ -4,7 +4,9 @@ function Game() {
         SNAKE_FILL_STYLE: 'lightBlue',
         SNAKE_STROKE_STYLE: 'darkBlue',
         SNAKE_PART_SIZE: 10,
-        SNAKE_MOVEMENT_SPEED: 100,
+        SNAKE_MOVEMENT_SPEED: 10,
+        SEED_FILL_STYLE: 'lightGreen',
+        SEED_STROKE_STYLE: 'darkGreen',
         BOARD_FILL_STYLE: 'white',
         BOARD_STROKE_STYLE: 'black',
         BOARD_WIDTH: 600,
@@ -35,7 +37,9 @@ function Game() {
     const context = gameBoard.getContext('2d');
 
     let snake = [  {x: 200, y: 200},  {x: 190, y: 200},  {x: 180, y: 200},  {x: 170, y: 200},  {x: 160, y: 200},];
-    let direction = DIRECTION.DOWN;
+    let score = 0;
+    let seed = {};
+    let direction = DIRECTION.RIGHT;
     let state = true;
 
     const drawSnakePart = (part) => {
@@ -53,19 +57,27 @@ function Game() {
         switch (ev.keyCode) {
             case KEY.LEFT:
             case KEY.A:
-                direction = DIRECTION.LEFT;
+                if (DIRECTION.RIGHT !== direction) {
+                    direction = DIRECTION.LEFT;
+                }
                 break;
             case KEY.UP:
             case KEY.W:
-                direction = DIRECTION.UP;
+                if (DIRECTION.DOWN !== direction) {
+                    direction = DIRECTION.UP;
+                }
                 break;
             case KEY.RIGHT:
             case KEY.D:
-                direction = DIRECTION.RIGHT;
+                if (DIRECTION.LEFT !== direction) {
+                    direction = DIRECTION.RIGHT;
+                }
                 break;
             case KEY.DOWN:
             case KEY.S:
-                direction = DIRECTION.DOWN;
+                if (DIRECTION.UP !== direction) {
+                    direction = DIRECTION.DOWN;
+                }
                 break;
         }
     }
@@ -87,15 +99,68 @@ function Game() {
                 break;
             case DIRECTION.DOWN:
                 head = {x: snake[0].x, y: snake[0].y + CONSTANTS.SNAKE_PART_SIZE};
+                break;
         }
-        if (head.x >= CONSTANTS.BOARD_WIDTH || head.y >= CONSTANTS.BOARD_HEIGHT
-        || head.x < 0 || head.y < 0) {
+        if (touchSeed(head, seed)) {
+            score++;
+            snake.push(growSnake(direction));
+            seed = spawnSeed();
+            console.log(score)
+        }
+        if (checkSnakeCollision(head)) {
+            document.removeEventListener("keydown", changeDirection);
             state = false;
-            document.removeEventListener("keydown", changeDirection)
             return;
         }
         snake.unshift(head);
         snake.pop();
+    }
+
+    const checkSnakeCollision = (head) => {
+        if (head.x >= CONSTANTS.BOARD_WIDTH || head.y >= CONSTANTS.BOARD_HEIGHT
+            || head.x < 0 || head.y < 0) {
+            return true;
+        }
+        return snake.some(item => item.x === head.x && item.y === head.y);
+    }
+
+    const growSnake = (direction) => {
+        const tail = snake.slice(-1)[0];
+        let growth;
+        switch (direction) {
+            case DIRECTION.RIGHT:
+                growth = {x: tail.x + CONSTANTS.SNAKE_PART_SIZE, y: tail.y};
+                break;
+            case DIRECTION.LEFT:
+                growth = {x: tail.x - CONSTANTS.SNAKE_PART_SIZE, y: tail.y};
+                break;
+            case DIRECTION.UP:
+                growth = {x: tail.x, y: tail.y - CONSTANTS.SNAKE_PART_SIZE};
+                break;
+            case DIRECTION.DOWN:
+                growth = {x: tail.x, y: tail.y + CONSTANTS.SNAKE_PART_SIZE};
+                break;
+        }
+        return growth;
+    }
+
+    const spawnSeed = () => {
+        const x = Math.round(Math.random() * CONSTANTS.BOARD_WIDTH / 10) * 10;
+        const y = Math.round(Math.random() * CONSTANTS.BOARD_HEIGHT / 10) * 10;
+        return {x, y}
+    }
+
+    const drawSeed = (seed) => {
+        context.fillStyle = CONSTANTS.SEED_FILL_STYLE;
+        context.strokestyle = CONSTANTS.SEED_STROKE_STYLE;
+        context.fillRect(seed.x, seed.y, CONSTANTS.SNAKE_PART_SIZE, CONSTANTS.SNAKE_PART_SIZE);
+        context.strokeRect(seed.x, seed.y, CONSTANTS.SNAKE_PART_SIZE, CONSTANTS.SNAKE_PART_SIZE);
+    }
+
+    const touchSeed = (head, seed) => {
+        if (head.x === seed.x && head.y === seed.y) {
+            return true;
+        }
     }
 
     const clearCanvas = () => {
@@ -108,12 +173,14 @@ function Game() {
     const gameRun = () => {
         clearCanvas();
         moveSnake(direction);
+        drawSeed(seed);
         drawSnake(snake);
         setTimeout(gameRun, CONSTANTS.SNAKE_MOVEMENT_SPEED)
     }
 
     this.init = () => {
         gameRun();
+        seed = spawnSeed();
         document.addEventListener("keydown", changeDirection)
     }
 
